d11669a20316c1a85df9dae579e4609484b79fd8
AZINIZD <93920464+AZINIZD@users.noreply.github.com>
2024-08-15 07:45:05 +0330

    Add files via upload

---
+++ D-Latch.py
@@ -0,0 +1,30 @@
+class DLatch:
+    def __init__(self):
+        self.D = 0
+        self.Clk = 0
+        self.Q = 0
+
+    def update(self):
+        if self.Clk:
+            self.Q = self.D
+
+# Example usage
+latch = DLatch()
+
+# Set D = 1, Clk = 1
+latch.D = 1
+latch.Clk = 1
+latch.update()
+print("After setting D=1, Clk=1:", latch.Q)  # Output should be 1
+
+# Set Clk = 0 (should not change Q)
+latch.D = 0
+latch.Clk = 0
+latch.update()
+print("After setting D=0, Clk=0:", latch.Q)  # Output should still be 1
+
+# Set Clk = 1 (should update Q to D)
+latch.D = 0
+latch.Clk = 1
+latch.update()
+print("After setting D=0, Clk=1:", latch.Q)  # Output should be 0
